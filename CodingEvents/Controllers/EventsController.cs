edab783fb98bfa6d82b310e523a8671715646b6a
Colleen Blake <colleenemilyblake@gmail.com>
2023-10-10 19:29:56 -0500

    Add project files.

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<string> Events = new()
+            { "Wedding", "Birthday", "Funeral" };
+            ViewBag.events = Events;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
